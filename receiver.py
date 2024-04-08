0ef94ae3ba7397adb3d252b7fd06e80a16770692
Oz174 <ahmedtarek1754@gmail.com>
2024-04-08 16:14:36 +0200

    added functions initialize and handle_messages

--- receiver.py
+++ receiver.py
@@ -2,42 +2,53 @@ import socket
 import time
 
 # Receiver setup
-receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-receiver_socket.bind(('localhost', 12345))
-receiver_socket.listen(1)
 
-print("Waiting for connection from sender...")
 
-# Accept connection from the sender
-sender_socket, sender_address = receiver_socket.accept()
-print("Connected to sender:", sender_address)
+def initiliaze_chat():
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    receiver_socket.bind(('localhost', 12345))
+    receiver_socket.listen(1)
 
-# Receive addition/subtraction problems from the sender and send back results
-while True:
-    msg = sender_socket.recv(1024).decode()
-    print("Message from sender:", msg)
-    # problem = sender_socket.recv(1024).decode()
-    # if not problem:
-    #     break
+    print("Waiting for connection from sender...")
 
-    if not msg:
-        print("Sender has left the chat , do you want to leave too? (y/n)")
-        ans = input(">")
-        if ans.lower() == "y":
-            print("Exiting ... ")
-            time.sleep(1)
-            exit()
+    # Accept connection from the sender
+    sender_socket, sender_address = receiver_socket.accept()
+    print("Connected to sender:", sender_address)
+    return sender_socket, receiver_socket
 
-    # # Process the problem and compute the result
-    # result = str(eval(problem))
-    reply = input("Type Here: ")
 
-    if not reply:
-        break
+def handle_messages(sender_socket, receiver_socket):
+    # Receive addition/subtraction problems from the sender and send back results
+    while True:
+        msg = sender_socket.recv(1024).decode()
+        print("Message from sender:", msg)
+        # problem = sender_socket.recv(1024).decode()
+        # if not problem:
+        #     break
 
-    # Send the result back to the sender
-    sender_socket.send(reply.encode())
+        if not msg:
+            print("Sender has left the chat , do you want to leave too? (y/n)")
+            ans = input(">")
+            if ans.lower() == "y":
+                print("Exiting ... ")
+                time.sleep(1)
+                exit()
 
-# Close the sockets
-sender_socket.close()
-receiver_socket.close()
+        # # Process the problem and compute the result
+        # result = str(eval(problem))
+        reply = input("Type Here: ")
+
+        if not reply:
+            break
+
+        # Send the result back to the sender
+        sender_socket.send(reply.encode())
+
+    # Close the sockets
+    sender_socket.close()
+    receiver_socket.close()
+
+
+if __name__ == "__main__":
+    sender_socket, receiver_socket = initiliaze_chat()
+    handle_messages(sender_socket, receiver_socket)
--- sender.py
+++ sender.py
@@ -1,30 +1,39 @@
 import socket
 
-# Sender setup
-sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connecting to the receiver running on the same machine
-sender_socket.connect(('localhost', 12345))
+
+def initiliaze_chat():
+    # Sender setup
+    sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connecting to the receiver running on the same machine
+    sender_socket.connect(('localhost', 12345))
+    return sender_socket
+
 
 # Send addition/subtraction problems to the receiver
-while True:
-    msg = input("Type Here: ")
-    # problem = input(
-    #     "Enter an addition/subtraction problem (e.g., '2+3' or '5-2'): ")
+def handle_messages(sender_socket):
+    while True:
+        msg = input("Type Here: ")
+        # problem = input(
+        #     "Enter an addition/subtraction problem (e.g., '2+3' or '5-2'): ")
 
-    # # Break if the user enters an empty string
-    # if not problem:
-    #     break
+        # # Break if the user enters an empty string
+        # if not problem:
+        #     break
 
-    if not msg:
-        break
+        if not msg:
+            break
 
-    sender_socket.send(msg.encode())
+        sender_socket.send(msg.encode())
 
-    # Receive the result from the receiver
-    # result = sender_socket.recv(1024).decode()
-    reply = sender_socket.recv(1024).decode()
-    print("Receiver Sent :", reply)
+        # Receive the result from the receiver
+        # result = sender_socket.recv(1024).decode()
+        reply = sender_socket.recv(1024).decode()
+        print("Receiver Sent :", reply)
+
+    # Close the sender socket
+    sender_socket.close()
 
 
-# Close the sender socket
-sender_socket.close()
+if __name__ == "__main__":
+    sender_socket = initiliaze_chat()
+    handle_messages(sender_socket)
