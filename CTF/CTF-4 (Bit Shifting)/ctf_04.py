89eb37bdc162cce5cc3272b1bbb0d499cdec382f
Oz174 <ahmedtarek1754@gmail.com>
2024-04-10 16:51:35 +0200

    fixed ctf4

--- CTF/CTF-4 (Bit Shifting)/ctf_04.py
+++ CTF/CTF-4 (Bit Shifting)/ctf_04.py
@@ -1,5 +1,5 @@
 def shift_circular_shemal(string, num_shifts):
-    return string[num_shifts-1:] + string[:num_shifts]
+    return string[num_shifts:] + string[:num_shifts]
 
 
 if __name__ == "__main__":
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     for letter in text:
         text_file_all_binary += '{0:08b}'.format(letter)
 
-    for i in range(2, 3):
+    for i in range(1, 2):
         shifted = shift_circular_shemal(text_file_all_binary, i)
     try:
         shifted = [chr(int(shifted[j:j+8], 2))
